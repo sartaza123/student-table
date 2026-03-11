d51aed35fda48c2ffc5a54ded82551716f74f15f
sartaza123 <sartaza123@gmail.com>
2026-03-11 20:25:01 +0530

    Header is created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,29 @@
-function Header() {
+import { FaUserGraduate, FaSearch } from "react-icons/fa";
+
+const Header = () => {
   return (
-    <>
-      <h1 className="text-red-400">react Header</h1>
-    </>
+    <header className="bg-[#1B0C1A] text-[#FADCD5] flex items-center justify-between px-8 py-4 shadow-lg">
+      {/* Logo + Title */}
+      <div className="flex items-center gap-3">
+        <FaUserGraduate size={26} />
+        <h1 className="text-xl font-semibold tracking-wide">
+          Students Manager
+        </h1>
+      </div>
+
+      {/* Search Bar */}
+      <div className="flex items-center bg-[#2D222F] px-3 py-2 rounded-md w-[300px]">
+        <input
+          type="text"
+          placeholder="Search students..."
+          className="bg-transparent outline-none text-sm w-full placeholder:text-gray-400"
+        />
+        <div className="text-[#FADCD5] cursor-pointer border-l px-2">
+          <FaSearch />
+        </div>
+      </div>
+    </header>
   );
-}
+};
+
 export default Header;
